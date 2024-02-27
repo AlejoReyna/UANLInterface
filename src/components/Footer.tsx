61fd16ce2a39b01f7ffd0a2a459dc37250a64d7d
Alexis Reyna <99472437+AlejoReyna@users.noreply.github.com>
2024-02-27 03:10:06 -0600

    Created Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+    return(
+        <footer>
+            <div className="vida-uni-contedor">
+                a
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
